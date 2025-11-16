f09b4e69f18de6f52767a528e4fa9a62bcbab10a
zobayerhb <hosenzobayer55@gmail.com>
2025-11-17 00:57:55 +0600

    problem 3 solve

--- solution.ts
+++ solution.ts
@@ -1,4 +1,4 @@
-// problem 1
+// PROBLEM 1
 type FormatValue = string | number | boolean;
 
 const formatValue = (values: FormatValue): FormatValue => {
@@ -10,6 +10,37 @@ const formatValue = (values: FormatValue): FormatValue => {
     return values === true ? false : true;
   }
 };
-console.log(formatValue("hello"));
-console.log(formatValue(5));
-console.log(formatValue(false));
+// console.log(formatValue("hello"));
+// console.log(formatValue(5));
+// console.log(formatValue(false));
+
+// PROBLEM 02
+type GetLength = string | any[];
+
+const getLength = (getLengthValues: GetLength) => {
+  if (typeof getLengthValues === "string") {
+    return getLengthValues.length;
+  } else if (Array.isArray(getLengthValues)) {
+    return getLengthValues.length;
+  }
+};
+// console.log(getLength(["Zobayer", 1, true, false, undefined, null]));
+// console.log(getLength("zobayer hosen"))
+
+
+// PROBLEM 03
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    (this.name = name), (this.age = age);
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+const getResult = new Person("Zobayer Hosen", 21);
+console.log(getResult.getDetails());
